0172a4315285b9f60bc2e683d82aff257186fa61
Leo Zhang <leoyzhang06@gmail.com>
2023-04-09 21:03:13 -0500

    Update Button.jsx

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Button({ children }) {
   return (
-    <button className='bg-cyan-700 hover:bg-cyan-800 text-white lg:text-2xl text-lg py-4 px-8 rounded-xl'>{children}</button>
+    <button className='w-fit bg-cyan-700 hover:bg-cyan-800 text-white text-md py-4 px-12 rounded-lg tracking-widest duration-300'>{children}</button>
   )
 }
