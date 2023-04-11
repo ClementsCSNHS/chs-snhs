8f48e634a6efd41cd16211e2bbd8fb37dc1d0170
Leo Zhang <leoyzhang06@gmail.com>
2023-04-11 13:58:49 -0500

    color of header and footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='bg-teal-800 w-full h-48'>Footer</div>
+    <div className='bg-slate-950 w-full h-48'>Footer</div>
   )
 }
