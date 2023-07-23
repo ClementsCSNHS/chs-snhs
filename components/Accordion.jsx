84e868631834b7c631b3b51799172ec0bc48f386
aaronthechen <aaronchen31415@gmail.com>
2023-07-22 20:03:46 -0500

    fix styling

--- components/Accordion.jsx
+++ components/Accordion.jsx
@@ -9,7 +9,7 @@ function Accordion({ items }) {
     <div className='text-xl'>
       {items.map((item, index) => (
         <div key={item.title}>
-          <button type="button" onClick={() => handleClick(index)} className="flex items-center justify-between w-full p-6 font-medium text-left text-gray-400 border border-gray-200  dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+          <button type="button" onClick={() => handleClick(index)} className="flex items-center justify-between w-full p-6 font-medium text-left text-white border focus:ring-gray-800 border-gray-700 hover:bg-gray-800" aria-expanded="true" aria-controls="accordion-collapse-body-1">
             <span>{item.title}</span>
             <svg className={`w-3 h-3 shrink-0 ${index === activeIndex && "rotate-180"}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='bg-slate-950 w-full py-2 px-10'>
-      <p className='text-gray-400 text-xs'>Website by <a className='hover:underline' href='https://chscsnhs.netlify.app/'>Clements CSNHS</a></p>
+      <p className='text-gray-400 text-sm'>Website by <a className='hover:underline' href='https://chscsnhs.netlify.app/'>Clements CSNHS</a></p>
     </div>
   )
 }
